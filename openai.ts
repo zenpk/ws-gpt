cf488c431d3fa1f4cb9453573e9556f36923de73
zenpk <99558412+zenpk@users.noreply.github.com>
2023-07-10 17:48:23 +0800

    Add answer from stackoverflow

--- openai.ts
+++ openai.ts
@@ -1,12 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
-import {
-  ChatCompletionRequestMessage,
-  CreateChatCompletionRequest,
-} from "openai/api";
-
-export async function chatGPTFake(message: string) {
-  return `hello from openai: ${message}`;
-}
+import { ChatCompletionRequestMessage } from "openai/api";
+import { IncomingMessage } from "http";
 
 export async function chatGPT(gptMessages: ChatCompletionRequestMessage[]) {
   const configuration = new Configuration({
@@ -15,14 +9,44 @@ export async function chatGPT(gptMessages: ChatCompletionRequestMessage[]) {
   });
   const openai = new OpenAIApi(configuration);
   try {
-    const body: CreateChatCompletionRequest = {
-      model: "gpt-3.5-turbo",
-      messages: gptMessages,
-    };
-    const resp = await openai.createChatCompletion(body);
-    const respBody = await resp.data;
-    return respBody.choices[0].message!.content;
+    const completion = await openai.createChatCompletion(
+      {
+        model: "gpt-3.5-turbo",
+        messages: gptMessages,
+        stream: true,
+      },
+      { responseType: "stream" },
+    );
+
+    const stream = completion.data as unknown as IncomingMessage;
+    stream.on("data", (chunk: Buffer) => {
+      const payloads = chunk.toString().split("\n\n");
+      for (const payload of payloads) {
+        if (payload.endsWith("[DONE]")) return;
+        if (payload.startsWith("data:")) {
+          const data = JSON.parse(payload.replace("data: ", ""));
+          try {
+            const chunk: undefined | string = data.choices[0].delta?.content;
+            if (chunk) {
+              console.log(chunk);
+            }
+          } catch (error) {
+            console.log(`Error with JSON.parse and ${payload}.\n${error}`);
+          }
+        }
+      }
+    });
+
+    stream.on("end", () => {
+      setTimeout(() => {
+        console.log("\nStream done");
+      }, 10);
+    });
+
+    stream.on("error", (err: Error) => {
+      console.log(err);
+    });
   } catch (e) {
-    return (e as Error).message;
+    console.log(e);
   }
 }
