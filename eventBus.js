5f239dc317400a9f85ef90d0caf9b7d8f411a433
zenpk <99558412+zenpk@users.noreply.github.com>
2023-07-17 10:58:27 +0800

    RM js

--- eventBus.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.eventName = exports.emitter = void 0;
-const node_events_1 = require("node:events");
-exports.emitter = new node_events_1.EventEmitter();
-exports.eventName = "newMessage";
