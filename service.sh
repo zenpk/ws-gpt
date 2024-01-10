16e6e09f8edc9c8d6c17ac0a1609e449ff71eced
zenpk <99558412+zenpk@users.noreply.github.com>
2024-01-11 01:17:31 +0800

    add script

---
+++ service.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+cd /absolute/path/to/my/project
+npm run start
